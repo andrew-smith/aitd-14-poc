81ad605780b89f2f2dd7bb11e0839f148574c82b
Andrew Smith <andrew@andrew.kiwi.nz>
2014-05-17 18:38:27 +1200

    Drawing red line on movements

--- app.js
+++ app.js
@@ -25,17 +25,51 @@ $(function() {
 
   var drawingLayer = new Kinetic.Layer();
 
+  var isMouseDown = false;
+  var lastCoords = {x: -1, y:-1};
 
-  stage.on('mousedown touchstart', function(event) {
-    console.debug("mousedown / touchstart");
-    var circle = new Kinetic.Circle({
-      radius: 15,
-      fill: 'red',
-      x: event.evt.layerX,
-      y: event.evt.layerY
-    });
-    drawingLayer.add(circle);
-    drawingLayer.draw();
+  stage.on('mousedown touchstart dragstart', function(event) {
+    console.debug(event.type);
+    isMouseDown = true;
+    lastCoords.x = event.evt.layerX;
+    lastCoords.y = event.evt.layerY;
+
+  });
+  stage.on('mouseup touchend mouseenter mouseleave dragend', function(event) {
+    console.debug(event.type);
+    isMouseDown = false;
+    lastCoords = {x: -1, y:-1};
+  });
+
+
+  stage.on('mousemove touchmove dragmove', function(event) {
+    console.debug(event.type);
+
+
+    if(isMouseDown && lastCoords.x !== -1){
+
+      var line = new Kinetic.Line({
+        stroke: 'red',
+        x:0,
+        y:0,
+        points: [event.evt.layerX,event.evt.layerY,lastCoords.x, lastCoords.y]
+
+      })
+      drawingLayer.add(line);
+
+
+      var circle = new Kinetic.Circle({
+        radius: 15,
+        fill: 'red',
+        x: event.evt.layerX,
+        y: event.evt.layerY
+      });
+      //drawingLayer.add(circle);
+      drawingLayer.draw();
+    }
+
+    lastCoords.x = event.evt.layerX;
+    lastCoords.y = event.evt.layerY;
   });
 
 
