cc530c936c9e276b11b4306fa0cd37b4c3e06e97
Andrew Smith <andrew@andrew.kiwi.nz>
2014-05-17 16:05:07 +1200

    Base HTML structure

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//Avoid undefines
+console.debug = console.debug || function() {};
+console.log = console.log || function() {};
+console.warn = console.warn || function() {};
+console.error = console.error || function() {};
+
+
+//main app
+
+$(function() {
+  console.debug("app started");
+
+
+});
