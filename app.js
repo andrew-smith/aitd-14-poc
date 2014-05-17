7d27243f74da635f9ec85a4ca5c306fd63362f12
Andrew Smith <andrew@andrew.kiwi.nz>
2014-05-17 18:11:25 +1200

    Basic canvas and drawing tool

--- app.js
+++ app.js
@@ -8,8 +8,53 @@ console.error = console.error || function() {};
 
 //main app
 
+var CANVAS_WIDTH = 800;
+var CANVAS_HEIGHT = 600;
+
 $(function() {
   console.debug("app started");
+
+
+  var stage = new Kinetic.Stage({
+    container: 'user-canvas',
+    width: CANVAS_WIDTH,
+    height: CANVAS_HEIGHT
+  });
+
+
+
+  var drawingLayer = new Kinetic.Layer();
+
+
+  stage.on('mousedown touchstart', function(event) {
+    console.debug("mousedown / touchstart");
+    var circle = new Kinetic.Circle({
+      radius: 15,
+      fill: 'red',
+      x: event.evt.layerX,
+      y: event.evt.layerY
+    });
+    drawingLayer.add(circle);
+    drawingLayer.draw();
+  });
+
+
+  var backgroundLayer = new Kinetic.Layer();
+  var backgroundImg = new Kinetic.Rect({
+    x: 0,
+    y: 0,
+    fill: 'blue',
+    width: CANVAS_WIDTH,
+    height : CANVAS_HEIGHT
+  });
+
+  backgroundLayer.add(backgroundImg);
+  stage.add(backgroundLayer);
+
+
+
+
+  stage.add(drawingLayer);
 
 
 });
